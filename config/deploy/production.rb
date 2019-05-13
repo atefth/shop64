0e90d7d4d99f8aedd13c3d99f55402195b5b0d74
Atef Haque <atefth@gmail.com>
2019-05-13 19:12:37 +0600

    removed devise.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :stage, :production
 set :rails_env, :production
-set :deploy_to, "/home/ec2-user/apps/shop64.co"
+set :deploy_to, "/home/ec2-user/apps/#{fetch(:application)}"
 set :branch, 'master'
 
 set :puma_user, fetch(:user)
 set :puma_rackup, -> { File.join(current_path, "config.ru") }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix:/#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log, "#{release_path}/log/puma.access.log"
